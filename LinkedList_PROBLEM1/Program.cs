f6de73f74d8c01e1aaa316c6c42cceb74f063077
Your Name <you@example.com>
2021-08-14 17:49:43 +0530

    feat:created simple linked list

--- LinkedList_PROBLEM1/Program.cs
+++ LinkedList_PROBLEM1/Program.cs
@@ -7,6 +7,13 @@ namespace LinkedList_PROBLEM1
         static void Main(string[] args)
         {
             Console.WriteLine("----------Welcome to Linked List Problem----------");
+            // use case 1
+            LinkedList list = new LinkedList { };
+            list.Add(56);
+            list.Add(30);
+            list.Add(70);
+            list.Display();
+
         }
     }
 }
